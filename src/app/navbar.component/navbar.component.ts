acb5ed747d6dae2f325ce928cf3b96763f54eddc
sanjay <sanjaymahesh345@gmail.com>
2026-07-29 12:02:17 +0530

    first commit

---
+++ src/app/navbar.component/navbar.component.ts
@@ -0,0 +1,43 @@
+import { Component, HostListener, signal } from '@angular/core';
+
+interface NavLink {
+  label: string;
+  href: string;
+}
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  readonly isMenuOpen = signal(false);
+  readonly isScrolled = signal(false);
+
+  readonly links: NavLink[] = [
+    { label: 'New In', href: '#new-in' },
+    { label: 'Women', href: '#women' },
+    { label: 'Men', href: '#men' },
+    { label: 'Collections', href: '#collections' },
+    { label: 'Sale', href: '#sale' },
+  ];
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((open) => !open);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    this.isScrolled.set(window.scrollY > 12);
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    this.closeMenu();
+  }
+}
